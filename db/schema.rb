64e07352d070a50e3ff4631441d0ca00b66e2ddf
Serkan Mutlu <serkmutlu@gmail.com>
2015-01-07 23:40:36 +0000

    Switch to mysql

--- db/schema.rb
+++ db/schema.rb
@@ -13,9 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20150104132332) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "choices", force: true do |t|
     t.string   "email"
     t.string   "shame"
